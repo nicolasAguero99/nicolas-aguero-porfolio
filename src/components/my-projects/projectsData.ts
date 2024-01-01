da3a959c5775dacb0738e28ae9bd7868ac410cfe
nicolasAguero99 <nico.agueroo99@gmail.com>
2024-01-01 17:29:46 -0300

    fondo destellos

--- src/components/my-projects/projectsData.ts
+++ src/components/my-projects/projectsData.ts
@@ -7,13 +7,13 @@ interface Project {
   codeLink: string
 }
 
-export type Technologies = 'React' | 'React native' | 'Node' | 'Mongo Db' | 'Axios' | 'Jwt' | 'Socket.io' | 'Scss' | 'Tailwind' | 'Three.js' | 'Html' | 'Css' | 'Javascript' | 'PWA' | 'API' | 'Bootstrap' | 'Php' | 'MySql' | 'Vue' | 'Firebase'
+export type Technologies = 'React' | 'React native' | 'Node' | 'Mongo Db' | 'Axios' | 'Jwt' | 'Socket.io' | 'SCSS' | 'Tailwind' | 'Three.js' | 'HTML' | 'CSS' | 'Javascript' | 'PWA' | 'API' | 'Bootstrap' | 'PHP' | 'MySql' | 'Vue' | 'Firebase'
 
 export const projects: Project[] = [
   {
     title: 'Ready',
     imageSrc: '/images/Ready.png',
-    description: 'Proyecto académico (tesis final) el cual consiste en una app que se centra en la gestión de tareas de las empresas. (Solicitar acceso para ver código).',
+    description: 'Proyecto académico (tesis). Es una plataforma de gestión de tareas y gamificación para empresas. (Solicitar acceso para ver código)',
     technologies: ['React', 'Node', 'Mongo Db', 'Axios', 'Jwt', 'Socket.io'],
     projectLink: 'https://ready-app-gamma.vercel.app/',
     codeLink: 'https://github.com/nicolasAguero99/Ready-project'
@@ -21,47 +21,47 @@ export const projects: Project[] = [
   {
     title: 'Nowly',
     imageSrc: '/images/Nowly.png',
-    description: 'Proyecto personal el cual consiste en una es una app mobile de administrador de tareas personal, el cual cuenta con diferentes funciones para ayudarte. (Solicitar acceso para ver código).',
+    description: 'Proyecto personal de una aplicación de organización, meditaciones y música para estudio y día a día. (Solicitar acceso para ver el código)',
     technologies: ['React native'],
     projectLink: '',
     codeLink: 'https://github.com/nicolasAguero99/Nowly-project'
   },
   {
     title: 'Task list',
-    imageSrc: '/images/TaskList.png',
-    description: 'Proyecto personal en el que consiste en una app de lista de tareas o también llamadas "task list" o "todo list".',
-    technologies: ['React', 'Scss'],
+    imageSrc: '/images/Task-List.png',
+    description: 'Proyecto personal en el que consiste en una app de lista de tareas o también llamadas "task list" o "todo list"',
+    technologies: ['React', 'SCSS'],
     projectLink: 'https://task-list-project-chi.vercel.app/',
     codeLink: 'https://github.com/nicolasAguero99/TaskList-project'
   },
   {
     title: 'Sneak-er',
-    imageSrc: '/images/SneakEr.png',
-    description: 'Proyecto personal en el que consiste en un prototipo de un sitio web, el cual se ofrecen zapatillas (calzado) deportivas de diferentes marcas.',
+    imageSrc: '/images/Sneak-Er.png',
+    description: 'Proyecto personal de un sitio web de una zapatería la cual ofrece calzado de diferentes marcas',
     technologies: ['React', 'Tailwind', 'Three.js'],
     projectLink: 'https://sneak-er.vercel.app/',
     codeLink: 'https://github.com/nicolasAguero99/SneakEr-project'
   },
   {
     title: 'Fly X',
-    imageSrc: '/images/FlyX.png',
-    description: 'Proyecto académico en el que consiste en una PWA (utilizando ServiceWorker y Manifest) y utilizando una API y un Json local.',
-    technologies: ['Html', 'Css', 'Javascript', 'Tailwind', 'PWA', 'API'],
+    imageSrc: '/images/Fly-X.png',
+    description: 'Proyecto académico de un sitio de viajes. Es una PWA (donde se utilizó ServiceWorker y Manifest),  basada en una API y un Json local',
+    technologies: ['HTML', 'CSS', 'Javascript', 'Tailwind', 'PWA', 'API'],
     projectLink: 'https://nicolasaguero99.github.io/FlyX-project/',
     codeLink: 'https://github.com/nicolasAguero99/FlyX-project'
   },
   {
     title: 'Tienda LevelUp',
-    imageSrc: '/images/LevelUp.png',
-    description: 'Proyecto académico en el que consiste realizar una tienda de juegos, que cuenta con una base de datos, sección para administradores, usuarios, login/register y carrito.',
-    technologies: ['Html', 'Css', 'Bootstrap', 'Php', 'MySql'],
+    imageSrc: '/images/Level-Up.png',
+    description: 'Proyecto académico de una tienda de juegos, que cuenta con una base de datos, sección para administradores, usuarios, login/register y carrito',
+    technologies: ['HTML', 'CSS', 'Bootstrap', 'PHP', 'MySql'],
     projectLink: 'http://level-up.rf.gd/',
     codeLink: 'https://github.com/nicolasAguero99/LevelUp-project'
   },
   {
     title: 'Creatimoon',
     imageSrc: '/images/Creatimoon.png',
-    description: 'Proyecto académico en el que consiste en realizar un sitio donde se ofrece el servicio de un equipo de diseñadores y programadores. Cuenta con el modo Administrador, para editar los precios en la página y una atención al cliente mediante mensaje.',
+    description: 'Proyecto académico de un sitio que ofrece servicios digitales tanto de programación como de diseño. Cuenta con mensajería de atención al cliente y un panel de administrador',
     technologies: ['Vue', 'Firebase'],
     projectLink: 'https://creatimoon-project.vercel.app/',
     codeLink: 'https://github.com/nicolasAguero99/Creatimoon-project'
@@ -69,15 +69,15 @@ export const projects: Project[] = [
   {
     title: 'Flixplay',
     imageSrc: '/images/Flixplay.png',
-    description: 'Proyecto académico en el que consiste en realizar una app de películas con su buscador, seleccionar una película en específico y ver sus detalles, empleando una API.',
+    description: 'Proyecto académico de una plataforma de películas con buscador, y poder ver los detalles de un filme en específico empleando una API',
     technologies: ['Vue', 'API'],
     projectLink: 'https://nicolasaguero99.github.io/Flixplay-project/',
     codeLink: 'https://github.com/nicolasAguero99/Flixplay-project'
   },
   {
     title: 'To Do App',
-    imageSrc: '/images/TodoApp.png',
-    description: 'Proyecto académico que consiste en realizar una todo app que cuenta con su buscador de tareas y sus funciones típicas de agregar, editar y borrar.',
+    imageSrc: '/images/To-Do-App.png',
+    description: 'Proyecto académico que consiste en realizar una “ToDo app” que cuenta con su buscador de tareas y sus funciones típicas de agregar, editar y borrar',
     technologies: ['Vue'],
     projectLink: 'https://nicolasaguero99.github.io/TodoApp-project/',
     codeLink: 'https://github.com/nicolasAguero99/TodoApp-project'
@@ -85,24 +85,24 @@ export const projects: Project[] = [
   {
     title: 'Urban Hell',
     imageSrc: '/images/Urban-Hell.png',
-    description: 'Proyecto académico en el que consiste en una tienda de ropa, con su catálogo de productos y poder agregar al carrito cada uno de estos.',
-    technologies: ['Html', 'Css', 'Javascript', 'PWA', 'API'],
+    description: 'Proyecto académico para una tienda de ropa, con su catálogo de productos y carrito de compras funcional',
+    technologies: ['HTML', 'CSS', 'Javascript', 'PWA', 'API'],
     projectLink: 'https://nicolasaguero99.github.io/UrbanHell-project/',
     codeLink: 'https://github.com/nicolasAguero99/UrbanHell-project'
   },
   {
     title: 'Starbucks landing page',
     imageSrc: '/images/Starbucks.png',
-    description: 'Proyecto académico que consiste en realizar una landing page promocionando la famosa empresa de Starbucks, a su vez contando con una PWA.',
-    technologies: ['Html', 'Css', 'Javascript', 'Bootstrap', 'PWA'],
+    description: 'Proyecto académico que consiste en realizar una landing page promocionando la famosa empresa de Starbucks, a su vez contando con una PWA',
+    technologies: ['HTML', 'CSS', 'Javascript', 'Bootstrap', 'PWA'],
     projectLink: 'https://nicolasaguero99.github.io/Starbucks-project/',
     codeLink: 'https://github.com/nicolasAguero99/Starbucks-project'
   },
   {
     title: 'Havanna landing page',
     imageSrc: '/images/Havanna.png',
-    description: 'Proyecto académico que consiste en realizar una landing page promocionando la famosa empresa Argentina de Havanna.',
-    technologies: ['Html', 'Css', 'Javascript', 'Bootstrap'],
+    description: 'Proyecto académico que consiste en realizar una landing page promocionando la famosa empresa Argentina de Havanna',
+    technologies: ['HTML', 'CSS', 'Javascript', 'Bootstrap'],
     projectLink: 'https://nicolasaguero99.github.io/Havanna-project/',
     codeLink: 'https://github.com/nicolasAguero99/Havanna-project'
   }
